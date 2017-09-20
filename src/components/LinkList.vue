7b853e6e91af153c0c2a6a3ae10ffad65d11b31f
Martin Peck <martinjohnpeck@gmail.com>
2017-09-20 20:21:51 +0100

    Add LinkList.vue (initial)

---
+++ src/components/LinkList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <link-item
+      v-for="link in allLinks"
+      :key="link.id"
+      :link="link">
+    </link-item>
+  </div>
+</template>
+
+<script>
+
+import LinkItem from './LinkItem'
+
+export default {
+  name: 'LinkList',
+  data () {
+    allLinks: [
+      {
+        id: '1',
+        description: 'The Coolest GraphQL Backend',
+        url: 'https://www.graph.cool'
+      }, {
+        id: '2',
+        description: 'The Best GraphQL Client',
+        url: 'http://dev.apollodata.com/'
+
+      }
+    ]
+  },
+  components: {
+    LinkItem
+  }
+}
+</script>
+
