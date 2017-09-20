0199428eb5971dbbbacc28e7f595895eac121ec4
Martin Peck <martinjohnpeck@gmail.com>
2017-09-20 20:41:45 +0100

    wire up LinkList to real GraphQL query

--- src/components/LinkList.vue
+++ src/components/LinkList.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h4 v-if="loading">Loading...</h4>
     <link-item
       v-for="link in allLinks"
       :key="link.id"
@@ -11,27 +12,23 @@
 <script>
 
 import LinkItem from './LinkItem'
+import { ALL_LINKS_QUERY } from '../constants/graphql'
 
 export default {
   name: 'LinkList',
   data () {
     return {
-      allLinks: [
-        {
-          id: '1',
-          description: 'The Coolest GraphQL Backend',
-          url: 'https://www.graph.cool'
-        }, {
-          id: '2',
-          description: 'The Best GraphQL Client',
-          url: 'http://dev.apollodata.com/'
-
-        }
-      ]
+      allLinks: [],
+      loading: 0
     }
   },
   components: {
     LinkItem
+  },
+  apollo: {
+    allLinks: {
+      query: ALL_LINKS_QUERY
+    }
   }
 }
 </script>
