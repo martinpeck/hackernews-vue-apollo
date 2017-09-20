d6857cfebb84da52baa6fbb80632d043afcd34cd
Martin Peck <martinjohnpeck@gmail.com>
2017-09-20 20:12:49 +0100

    wire up apollo client

--- src/main.js
+++ src/main.js
@@ -1,15 +1,36 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+import { ApolloClient, createBatchingNetworkInterface } from 'apollo-client'
+import VueApollo from 'vue-apollo'
+
 import 'tachyons'
 
 Vue.config.productionTip = false
 
+const networkInterface = createBatchingNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj7t4et2o0n210111lt1ipxt9'
+})
+
+const apolloClient = new ApolloClient({
+  networkInterface,
+  connectToDevTools: true
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  apolloProvider,
   router,
   template: '<App/>',
   components: { App }
