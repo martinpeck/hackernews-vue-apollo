e83b2e11ad9e0a1566eaf6ced9b10388299188f4
Martin Peck <martinjohnpeck@gmail.com>
2017-09-20 20:49:24 +0100

    initial CreateLink view

---
+++ src/components/CreateLink.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="flex flex-column mt3">
+      <input
+        class="mb2"
+        v-model="description"
+        type="text"
+        placeholder="A description for the link">
+      <input
+        class="mb2"
+        v-model="url"
+        type="text"
+        placeholder="The URL for the link">
+      <button @click="createLink">Submit</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateLink',
+  data () {
+    return {
+      description: '',
+      url: ''
+    }
+  },
+  methods: {
+    createLink () {
+      // ... stuff
+    }
+  }
+
+}
+</script>
