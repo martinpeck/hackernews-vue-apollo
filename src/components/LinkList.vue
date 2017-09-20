aa949f91104cc63e1cb4dec2ad0573384c08428e
Martin Peck <martinjohnpeck@gmail.com>
2017-09-20 20:24:32 +0100

    fix issue with data

--- src/components/LinkList.vue
+++ src/components/LinkList.vue
@@ -15,18 +15,20 @@ import LinkItem from './LinkItem'
 export default {
   name: 'LinkList',
   data () {
-    allLinks: [
-      {
-        id: '1',
-        description: 'The Coolest GraphQL Backend',
-        url: 'https://www.graph.cool'
-      }, {
-        id: '2',
-        description: 'The Best GraphQL Client',
-        url: 'http://dev.apollodata.com/'
+    return {
+      allLinks: [
+        {
+          id: '1',
+          description: 'The Coolest GraphQL Backend',
+          url: 'https://www.graph.cool'
+        }, {
+          id: '2',
+          description: 'The Best GraphQL Client',
+          url: 'http://dev.apollodata.com/'
 
-      }
-    ]
+        }
+      ]
+    }
   },
   components: {
     LinkItem
