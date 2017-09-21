6a994ebb857e616a9a4c0f421b3b7929c7bac1d1
Martin Peck <martinjohnpeck@gmail.com>
2017-09-21 11:41:50 +0100

    add login and createuser mutations

--- src/constants/graphql.js
+++ src/constants/graphql.js
@@ -24,3 +24,42 @@ export const CREATE_LINK_MUTATION = gql`
     }
   }
 `
+
+export const CREATE_USER_MUTATION = gql`
+  mutation CreateUserMutation($name: String!, $email: String!, $password: $String!) {
+    createUser(
+      name: $name,
+      authProvider: {
+        email: {
+          email: $email,
+          password: $password
+        }
+      }
+    ) {
+      id
+    }
+
+    signinUser(email: {
+      email: $email,
+      password: $password
+    }) {
+      token
+      user {
+        id
+      }
+    }
+`
+
+export const SIGNIN_USER_MUTATION = gql`
+  mutation SigninUserMutation(£email: String!, $password: String!) {
+    signinUser(email: {
+      email: $email,
+      password: $password
+    }) {
+      token
+      user {
+        id
+      }
+    }
+  }
+`
