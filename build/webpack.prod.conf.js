82672c3c3654c0151d6f34b8de119db0cb0ed4c8
Martin Peck <martinjohnpeck@gmail.com>
2017-09-21 21:56:07 +0100

    add CNAME and copy it during build

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -95,6 +95,10 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../CNAME'),
+        to: config.build.assetsRoot
       }
     ])
   ]
